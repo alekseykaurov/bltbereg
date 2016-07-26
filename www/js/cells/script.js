d528a713981f4e728d69edd3e6dd5354d05d3ef0
KatyHouse <xomka30098@gmail.com>
2016-07-26 23:15:55 +0300

    Рисование ручки и фиксики

--- www/js/cells/script.js
+++ www/js/cells/script.js
@@ -25,6 +25,7 @@ $(document).ready(function() {
   var lines_1 = new Array();
   var lines_2 = new Array();
   var lugs = new Array();
+  var handle = new fabric.Circle();
 
   if(product==undefined && project==undefined){
     // window.location.pathname = "/";
@@ -88,7 +89,8 @@ $(document).ready(function() {
     order.quantity_cells = quantity_cells;
     canvas.clear();
     countProportion(quantity_row, quantity_cells);
-    console.log(order);
+    $('.cell_image').show();
+    getPrice();
   }
 
   defaultLoad(product);
@@ -104,12 +106,30 @@ $(document).ready(function() {
 
         $(".setting_value.main_color_type").html(data["main_color_type"]["pagetitle"]);
         $(".setting_value.main_color").html(data["main_color"]["pagetitle"]);
+
+      }
+    });
+  }
+
+  function getPrice(){
+
+  	$.ajax({
+      url: '/ajax_cells/getPrice.php',
+      type: 'POST',
+      dataType: 'json',
+      async: false,
+      data: {'order': order},
+      success: function(data){
         
-        console.log("get names");
+        var str = String(data["total"]);
+		str = str.replace(/(\d)(?=(\d\d\d)+([^\d]|$))/g, '$1 ');
+        $(".price").html(str + '=');
+        console.log("get price");
         console.log(data);
 
       }
     });
+  	
   }
 
   // var width_cells_all = total_width - 90;
@@ -147,7 +167,14 @@ $(document).ready(function() {
     });
     canvas.add(rect);
     rects[count] = rect;
-    if (order.bars_type == 4){
+    if (order.bars_type == 1 || order.bars_type == 2){
+      if (count == 1){
+        drawLug(rect_height, rect_width, rect_left + 2, 0);
+        drawHandle(rect_height, rect_left);
+      }else if (count == 2){
+        drawLug(rect_height, rect_width, rect_left, 1);
+      }
+    }else if (order.bars_type == 4 || order.bars_type == 3){
       if (count == 2){
         drawLug(rect_height, rect_width, rect_left + 2, 0);
       }else if(count == 3){
@@ -156,6 +183,19 @@ $(document).ready(function() {
     }else{
       canvas.renderAll();
     }
+  }
+  function drawHandle(rect_height, rect_left){
+    handle.set({
+      top: rect_height + 11,
+      left: rect_left - 3,
+      radius: 4,
+      fill: '#fff',
+      stroke: '#ccc',
+      strokeWidth: 5,
+      selectable: false
+    });
+    canvas.add(handle);
+    canvas.renderAll();
   }
   function drawLug(rect_height, rect_width, rect_left, count){
     var lug = new fabric.Rect({
@@ -345,8 +385,6 @@ $(document).ready(function() {
     $("#width_cell_"+whoIsChecked).prop("checked",true);
     order.cell_width = widths[whoIsChecked-1];
     countPossibleHeights(widths[whoIsChecked-1] - 15);
-    console.log("ff");
-    console.log(widths);
 
     return widths;
   }
@@ -377,13 +415,11 @@ $(document).ready(function() {
       a = height_2/2;
       height_2 = Math.floor(Math.sqrt(4*a*a - current_width*current_width));
       if (height_2 >= 400 && height_2 < order.total_height){
-      $(".height-visible-possible").append('<label for="height_cell_2"> <span class="possible_height_2" data-quantityrow = "'+quantity_row_2+'">'+(height_2 - 15)+'</span><input type="radio" name="height_cell" id="height_cell_2"></label>');           
-      if(!flag){
-        order.cell_height = height_2;
-        $("#height_cell_2").prop("checked",true);
-      }
-      console.log("hahahah");
-      console.log(order);
+      	$(".height-visible-possible").append('<label for="height_cell_2"> <span class="possible_height_2" data-quantityrow = "'+quantity_row_2+'">'+(height_2 - 15)+'</span><input type="radio" name="height_cell" id="height_cell_2"></label>');           
+		if(!flag){
+			order.cell_height = height_2;
+			$("#height_cell_2").prop("checked",true);
+		}
       }
     }
   }
@@ -401,17 +437,18 @@ $(document).ready(function() {
     rect_height = cells_height;
     if (order.bars_type == 1){
       cells_width = (total_width - 90)/pp;
-      drawVertical(((90/3)*2)/pp, rect_height, 0, 0);
-      drawVertical((90/3)/pp, rect_height, ((90/3)*2)/pp + cells_width, 1);
+      drawVertical((90/3)/pp, rect_height, 0, 0);
+      drawVertical((90/3)/pp, rect_height, (90/3)/pp + cells_width, 1);
+      drawVertical((90/3)/pp, rect_height, ((90/3)*2)/pp + cells_width + 1, 2);
       if (order.total_height - 20 < order.cell_width*2.8*1.5){
         order.prolety = 3;
-        drawCells_2_2(cells_width, cells_height, ((90/3)*2)/pp, lines_1, quantity_row, quantity_cells);
+        drawCells_2_2(cells_width, cells_height, (90/3)/pp, lines_1, quantity_row, quantity_cells);
       }else if (order.total_height - 20 < order.cell_width*2.8*2){
         order.prolety = 4;
-        drawCells_2_3(cells_width, cells_height, ((90/3)*2)/pp, lines_1, quantity_row, quantity_cells);
+        drawCells_2_3(cells_width, cells_height, (90/3)/pp, lines_1, quantity_row, quantity_cells);
       }else{
         order.prolety = quantity_row*2 + 1;
-        drawCells(cells_width, cells_height, ((90/3)*2)/pp, lines_1, quantity_row, quantity_cells);
+        drawCells(cells_width, cells_height, (90/3)/pp, lines_1, quantity_row, quantity_cells);
       }
     }else if (order.bars_type == 2){
       cells_width = ((total_width - 60)/2)/pp;
@@ -436,7 +473,8 @@ $(document).ready(function() {
       cells_width = (total_width - 120)/pp;
      drawVertical((90/3)/pp, rect_height, 0, 0);
      drawVertical((90/3)/pp, rect_height, (90/3)/pp + 1, 1);
-     drawVertical((90/3)/pp, rect_height, ((90/3)*2)/pp + cells_width + 1, 2);
+     drawVertical((90/3)/pp, rect_height, (90/3)*2/pp + cells_width + 1, 2);
+     drawVertical((90/3)/pp, rect_height, 90/pp + cells_width + 2, 3);
      if (order.total_height - 20 < order.cell_width*2.8*1.5){
         order.prolety = 3;
         drawCells_2_2(cells_width, cells_height, ((90/3)*2)/pp + 1, lines_1, quantity_row, quantity_cells);
@@ -539,24 +577,23 @@ $(document).ready(function() {
       }
 
       if(!error){
-        order.total_height = tot_height;
-      order.total_width = tot_width;
-      countPossibleWidths();
-      var quantity_row = Number($("input[name='height_cell']:checked").prev().data("quantityrow"));
-      var quantity_cells = Number($("input[name='width_cell']:checked").prev().data("quantitycells"));
-      canvas.clear();
-      order.quantity_cells = quantity_cells;
-      countProportion(quantity_row, quantity_cells);
+		order.total_height = tot_height;
+		order.total_width = tot_width;
+		countPossibleWidths();
+		var quantity_row = Number($("input[name='height_cell']:checked").prev().data("quantityrow"));
+		var quantity_cells = Number($("input[name='width_cell']:checked").prev().data("quantitycells"));
+		canvas.clear();
+		order.quantity_cells = quantity_cells;
+		countProportion(quantity_row, quantity_cells);
 
-      $(".height-value").html(order.total_height);
-      $(".width-value").html(order.total_width);
+		$(".height-value").html(order.total_height);
+		$(".width-value").html(order.total_width);
 
-      $(".wrapper").hide();
-      console.log("width-height");
-      console.log(order);
+		$(".wrapper").hide();
+
+		getPrice();
+
       }
-
-    
 
   });
 
@@ -573,8 +610,8 @@ $(document).ready(function() {
       order.quantity_cells = quantity_cells;
       countProportion(quantity_row, quantity_cells);
 
-      console.log("type");
-      console.log(order);
+      getPrice();
+
   });
 
   //нажатие на выбор ширины
@@ -586,6 +623,8 @@ $(document).ready(function() {
     canvas.clear();
     order.quantity_cells = quantity_cells;
     countProportion(quantity_row, quantity_cells);
+
+    getPrice();
   });
 
   //нажатие на выбор высоты
@@ -596,56 +635,58 @@ $(document).ready(function() {
     order.quantity_cells = quantity_cells;
     canvas.clear();
     countProportion(quantity_row, quantity_cells);
-    console.log(order);
+
+    getPrice();
   });
 
   //нажатие на меню
 
   $(".setting_value").click(function(){
-    var type_id = $(this).data('pageid');
-  var classList = $(this).attr('class').split(/\s+/);
-  $.each(classList, function(index, item){
-    for (var i = 0; i < classList.length; i++){
-      if (classList[i] != 'setting_value' && classList[i] != 'active'){
-        type = classList[i];
-      }
-    }
-  });
+	var type_id = $(this).data('pageid');
+	var classList = $(this).attr('class').split(/\s+/);
+	$.each(classList, function(index, item){
+		for (var i = 0; i < classList.length; i++){
+		  if (classList[i] != 'setting_value' && classList[i] != 'active'){
+		    type = classList[i];
+		  }
+		}
+  	});
 
-  if(canChange!=false && isChange[type] != false){
+  	if(canChange!=false && isChange[type] != false){
 
-    $('.setting_value').removeClass('active');
-    $(this).addClass('active');
+	    $('.setting_value').removeClass('active');
+	    $(this).addClass('active');
 
-    $('.option_name').removeClass('active');
-    $('.option_name'+'.'+type).addClass('active');
-    $('.option_settings').removeClass('active');
-    $('.option_settings'+'.'+type).addClass('active');
-    $('.current_menu').remove();
-    $(this).parent().append('<div class = "current_menu"></div>');
-    $('.close_div').remove();
-    $('.current_menu').before('<div class = "close_div"><img class = "close" src = "/images/metalcount/close.gif" /></div>');
-    // $('.current_menu').append('<div class = "zamok_sort_title">Сортировка по цене:</div><form class = "zamok_sort"><select name="sort"><option value="up">По возр.</option><option value="down">По убыв.</option></select></form>');
+	    $('.option_name').removeClass('active');
+	    $('.option_name'+'.'+type).addClass('active');
+	    $('.option_settings').removeClass('active');
+	    $('.option_settings'+'.'+type).addClass('active');
+	    $('.current_menu').remove();
+	    $(this).parent().append('<div class = "current_menu"></div>');
+	    $('.close_div').remove();
+	    $('.current_menu').before('<div class = "close_div"><img class = "close" src = "/images/metalcount/close.gif" /></div>');
+	    // $('.current_menu').append('<div class = "zamok_sort_title">Сортировка по цене:</div><form class = "zamok_sort"><select name="sort"><option value="up">По возр.</option><option value="down">По убыв.</option></select></form>');
 
-    $.ajax({
-      url: '/ajax_cells/get_menu.php',
-      type: 'POST',
-      dataType: 'json',
-      data: {
-        'page': type_id,
-        'type': type
-      },
-      success: function(data){
-        // $('.current_menu').html('');
-        // alert(type);
-        console.log(data);
-        $('.current_menu h2').append(data['page']['pagetitle']);
-        $('.current_menu').append(data['txt']);
-        $(".current_menu div[data-pageid="+order[type]+"]").addClass("active-child");
-      }
-    });
+	    $.ajax({
+	      url: '/ajax_cells/get_menu.php',
+	      type: 'POST',
+	      dataType: 'json',
+	      data: {
+	        'page': type_id,
+	        'type': type
+	      },
+	      success: function(data){
+	        // $('.current_menu').html('');
+	        // alert(type);
+	        console.log("get_menu");
+	        console.log(data);
+	        $('.current_menu h2').append(data['page']['pagetitle']);
+	        $('.current_menu').append(data['txt']);
+	        $(".current_menu div[data-pageid="+order[type]+"]").addClass("active-child");
+	      }
+	    });
 
-  }
+  	}
 
   });
 
@@ -665,39 +706,54 @@ $(document).ready(function() {
       return false;
     }else{
 
-      var type_id = $(this).data('pageid');
-      order[type] = type_id;
+		var type_id = $(this).data('pageid');
+		var color;
+		order[type] = type_id;
 
-      $(".current_menu div").removeClass("active-child");
-      $(".current_menu div[data-pageid="+order[type]+"]").addClass("active-child");
+		$(".current_menu div").removeClass("active-child");
+		$(".current_menu div[data-pageid="+order[type]+"]").addClass("active-child");
 
-      if(type=="main_color_type"){
-        if($(this).attr('id')=="standart"){
-          order["main_color"] = 197;
-          $(".setting_value.main_color").data("pageid","196");
-        } else {
-          order["main_color"] = 201;
-          $(".setting_value.main_color").data("pageid","200");
-        }
-      } else {
-        // alert($(this).children(".color_color_ral").css("background-color"));
-        var color = $(this).children(".color_color_ral").css("background-color");
-        var quantity_rects = rects.length;
-        for (i = 0; i < quantity_rects; i++){
-          rects[i].set({fill: color});
-        }
-        var quantity_lines_1 = lines_1.length;
-        var quantity_lines_2 = lines_2.length;
-        for (i = 0; i<quantity_lines_1; i++){
-          lines_1[i].set({stroke: color});
-        }
-        for (i = 0; i<quantity_lines_2; i++){
-          lines_2[i].set({stroke: color});
-        }
-        canvas.renderAll();
-      }
-      
-      fillPole();
+		if(type=="main_color_type"){
+			if($(this).attr('id')=="standart"){
+				order["main_color"] = 197;
+				$(".setting_value.main_color").data("pageid","196");
+				color = "#3d2219";
+			} else {
+				order["main_color"] = 201;
+				$(".setting_value.main_color").data("pageid","200");
+				color = "#C2B078";
+			}
+		} else {
+		// alert($(this).children(".color_color_ral").css("background-color"));
+		color = $(this).children(".color_color_ral").css("background-color");
+
+		}
+
+		var quantity_rects = rects.length;
+		for (i = 0; i < quantity_rects; i++){
+			rects[i].set({fill: color});
+		}
+		var quantity_lines_1 = lines_1.length;
+		var quantity_lines_2 = lines_2.length;
+		for (i = 0; i<quantity_lines_1; i++){
+			lines_1[i].set({stroke: color});
+		}
+		for (i = 0; i<quantity_lines_2; i++){
+			lines_2[i].set({stroke: color});
+		}
+    handle.set({stroke: color});
+		canvas.renderAll();
+		  
+		fillPole();
     }
   });
+
+  //для теста
+  $(".load_order_title").click(function(){
+  	console.log("order");
+  	console.log(order);
+  	getPrice();
+  });
+
+
 })
